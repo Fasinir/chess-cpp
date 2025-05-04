ad40ba2b98d23f840a6ab8d58b4b61fa8157204a
Fasinir <filipson17@gmail.com>
2025-05-04 14:18:00 +0200

    refactor packages

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 #include <ostream>
 
-#include "model/board/ChessBoard.h"
+#include "src/model/board/ChessBoard.h"
 #include "LocalPrintTesting.h"
 
 
 int main(int argc, char *argv[]) {
     std::unique_ptr<ChessBoard> chessBoard = ChessBoard::STANDARD_BOARD();
     //std::cout << *chessBoard << std::endl;
-    //LocalPrintTesting::printMove();
-    //LocalPrintTesting::viewFigures();
-    //LocalPrintTesting::applyDefaultMove(*chessBoard);
-    //LocalPrintTesting::castlingOne(*chessBoard);
-    //LocalPrintTesting::castlingTwo(*chessBoard);
-    //LocalPrintTesting::castlingThree(*chessBoard);
-    //LocalPrintTesting::castlingFour(*chessBoard);
-    //LocalPrintTesting::enPassantOne(*chessBoard);
+    // TODO: make an actual test
+    LocalPrintTesting::printMove();
+    LocalPrintTesting::viewFigures();
+    LocalPrintTesting::applyDefaultMove(*chessBoard);
+    LocalPrintTesting::castlingOne(*chessBoard);
+    LocalPrintTesting::castlingTwo(*chessBoard);
+    LocalPrintTesting::castlingThree(*chessBoard);
+    LocalPrintTesting::castlingFour(*chessBoard);
+    LocalPrintTesting::enPassantOne(*chessBoard);
     LocalPrintTesting::enPassantTwo(*chessBoard);
-    //LocalPrintTesting::enPassantThree(*chessBoard);
-    //LocalPrintTesting::enPassantFour(*chessBoard);
-    //LocalPrintTesting::printCoordinates();
-    //LocalPrintTesting::legalMovesForStandardBoard();
-    //LocalPrintTesting::legalMovesForBishop();
-    //LocalPrintTesting::legalMovesForRook();
-    //LocalPrintTesting::legalMovesForQueen();
-    //LocalPrintTesting::legalMovesForKing();
-    //LocalPrintTesting::legalMovesForPawn();
-    //LocalPrintTesting::legalMovesForKingCastle();
+    LocalPrintTesting::enPassantThree(*chessBoard);
+    LocalPrintTesting::enPassantFour(*chessBoard);
+    LocalPrintTesting::printCoordinates();
+    LocalPrintTesting::legalMovesForStandardBoard();
+    LocalPrintTesting::legalMovesForBishop();
+    LocalPrintTesting::legalMovesForRook();
+    LocalPrintTesting::legalMovesForQueen();
+    LocalPrintTesting::legalMovesForKing();
+    LocalPrintTesting::legalMovesForPawn();
+    LocalPrintTesting::legalMovesForKingCastle();
     return 0;
 }
